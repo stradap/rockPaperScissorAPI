2f43963be6f162d7fd4acdb81d677fdd5d41a812
Juan Pablo Estrada <pabloleeestrada@gmail.com>
2016-05-17 02:12:50 +0000

    Added the top result

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ String.prototype.equalsIgnoreCase = function(otherString) {
 	return (this.toUpperCase() == otherString.toUpperCase());
 }
 api.use(bodyParser.json());
-api.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+api.use(bodyParser.urlencoded({
+	extended: true
+})); // support encoded bodies
 
 api.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -35,19 +37,24 @@ var storage = multer.diskStorage({
 var upload = multer({
 	storage: storage
 }).single('champFile');
+
 //
 api.post('/api/championship/new', function(req, res) {
 	upload(req, res, function(err) {
 		if (err) {
-			return res.end("Error uploading file.");		}
+			return res.end("Error uploading file.");
+		}
 		var result = algorithm.extractGames(req.file.path);
 		var data = algorithm.championship(result);
 		var firstSecond = data.split(",");
-		var save = algorithm.saveHistory(data ,	plGame = require("./models/playerModel"));
+		var save = algorithm.saveHistory(data, plGame = require("./models/playerModel"));
 		res.end("The winner of the tournament is: " + firstSecond[0] + "," + firstSecond[1] + " and all the data was storage");
 	});
 });
-//
+
+/*Stores the first and second place of a tournament, each user is stored with their respective scores. 
+The user names will be unique, but the same user can win 1 or more championships. 
+Returns the operation status if successfully.*/
 api.post('/api/championship/result', function(req, res) {
 	var firstWinner = "";
 	var secondWinner = "";
@@ -60,18 +67,29 @@ api.post('/api/championship/result', function(req, res) {
 	if (firstWinner && secondWinner) {
 		algorithm.saveToHistory(firstWinner, secondWinner, plGame);
 		res.end("success");
-	}else{
+	}
+	else {
 		res.end("fail");
 	}
 });
 
 //Retrieves the top players of all championships. Returns the list of player names based on the count provided. By default 10
 api.get('/api/championship/top/', function(req, res) {
-		var count = 10;
-		if(req.query.count){
-			count = req.query.count;
-		}
-		res.json({ Count: count });
+	var count = 10;
+	if (req.query.count) {
+		count = req.query.count;
+	}
+	var result = algorithm.getTop(count, plGame);
+
+	result.then(function(data) {
+		data = data.map(function(user) {
+			return user.namePlayer;
+
+		});
+		var result = data;
+
+		res.send(result);
+	});
 });
 
 api.get('/', function(req, res) {
